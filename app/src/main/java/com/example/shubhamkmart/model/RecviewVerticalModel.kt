c0d121c7b80ab1c49e8fa2cedcde70d35130e4e4
Chouhan <chouhandheeraj8@gmail.com>
2023-09-30 15:50:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shubhamkmart/model/RecviewVerticalModel.kt
@@ -0,0 +1,8 @@
+package com.example.shubhamkmart.model
+
+data class RecviewVerticalModel(
+    val imgFav : Int,
+    val imgProduct :Int,
+    val txtName :String,
+    val txtPrice :String
+)
---
+++ app/src/main/java/com/example/shubhamkmart/screen/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shubhamkmart.screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shubhamkmart.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
